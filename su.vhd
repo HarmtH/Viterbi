6141fb042a8943290af70e99f808780c09c61bdf
HarmtH <harm@tehennepe.org>
2014-09-22 18:38:52 +0200

    State unit omgebouwd naar for-loops.
    Change-Id: I2af3cb18f9ef9babd8f9f5ef703c922068405982

--- su.vhd
+++ su.vhd
@@ -15,8 +15,8 @@ entity su is
 		in_state0    : in  state_type;
 		in_state1    : in  state_type;
 		out_state    : out state_type;
-		in_tb0       : in tb_type;
-		in_tb1       : in tb_type;
+		in_tb0       : in  tb_type;
+		in_tb1       : in  tb_type;
 		out_tb       : out tb_type;
 		input_stream : in  input_array;
 		trans_val0   : in  trans_type;
@@ -27,58 +27,50 @@ end entity su;
 architecture bhv of su is
 begin
 	P0 : process(clk, rst) is
-		variable trans_val_dec_path1 : signed(3 downto 0);
-		variable trans_val_dec_path2 : signed(3 downto 0);
-		variable path1               : state_array := (others => 0);
-		variable path2               : state_array := (others => 0);
-		variable path1_total         : state_type;
-		variable path2_total         : state_type;
+		type path_array is array (0 to 1) of state_array;
+		type trans_val_dec_array is array (0 to 1) of signed(3 downto 0);
+		--variable trans_val_dec_path1 : signed(3 downto 0);
+		--variable trans_val_dec_path2 : signed(3 downto 0);
+		variable trans_val_dec : trans_val_dec_array;
+		variable path          : path_array := (others => (others => 0));
+		variable path_total    : state_array;
 	begin
 		if rst = '1' then
 			out_state <= 0;
 			out_tb    <= (others => '0');
 		elsif rising_edge(clk) then
-			trans_val_dec_path1 := to_signed(trans_val0, trans_val_dec_path1'length);
-			trans_val_dec_path2 := to_signed(trans_val1, trans_val_dec_path2'length);
+			trans_val_dec(0) := to_signed(trans_val0, trans_val_dec(0)'length);
+			trans_val_dec(1) := to_signed(trans_val1, trans_val_dec(1)'length);
 			if (rate = 1) then
-				path1(0)          := (input_stream(0) - trans_val0) * (input_stream(0) - trans_val0);
-				path1(3 downto 1) := (others => 0);
-				path2(0)          := (input_stream(0) - trans_val1) * (input_stream(0) - trans_val1);
-				path2(3 downto 1) := (others => 0);
-			elsif (rate = 2) then
-				path1(3 downto 2) := (others => 0);
-				path1(3 downto 2) := (others => 0);
-				
-				if trans_val_dec_path1(0) = '0' then
-					path1(0) := input_stream(0);
-				else
-					path1(0) := -input_stream(0);
-				end if;
-				if trans_val_dec_path1(1) = '0' then
-					path1(1) := input_stream(1);
-				else
-					path1(1) := -input_stream(1);
-				end if;
-				if trans_val_dec_path2(0) = '0' then
-					path2(0) := input_stream(0);
-				else
-					path2(0) := -input_stream(0);
-				end if;
-				if trans_val_dec_path2(1) = '0' then
-					path2(1) := input_stream(1);
-				else
-					path2(1) := -input_stream(1);
-				end if;
-			else
-				report "error" severity error;
+				path(0)(0)          := (input_stream(0) - trans_val0) * (input_stream(0) - trans_val0);
+				path(0)(3 downto 1) := (others => 0);
+				path(1)(0)          := (input_stream(0) - trans_val1) * (input_stream(0) - trans_val1);
+				path(1)(3 downto 1) := (others => 0);
+			else 
+				path(0)(3 downto rate) := (others => 0);
+				path(1)(3 downto rate) := (others => 0);
+				for i in 0 to (rate - 1) loop
+					for j in 0 to 1 loop
+						if trans_val_dec(j)(i) = '0' then
+							path(j)(i) := input_stream(i);
+						else
+							path(j)(i) := -input_stream(i);
+						end if;
+					end loop;
+				end loop;
 			end if;
-			path1_total := in_state0 + path1(0) + path1(1) + path1(2) + path1(3);
-			path2_total := in_state1 + path2(0) + path2(1) + path2(2) + path2(3);
-			if (path1_total < path2_total) or count < 2 then
-				out_state <= path1_total;
+			path_total(0) := in_state0;
+			path_total(1) := in_state1;
+			for i in 0 to 3 loop
+				for j in 0 to 1 loop
+					path_total(j) := path_total(j) + path(j)(i);
+				end loop;
+			end loop;
+			if (path_total(0) < path_total(1)) or count < 2 then
+				out_state <= path_total(0);
 				out_tb    <= bit & in_tb0(7 downto 1);
 			else
-				out_state <= path2_total;
+				out_state <= path_total(1);
 				out_tb    <= bit & in_tb1(7 downto 1);
 			end if;
 		end if;
